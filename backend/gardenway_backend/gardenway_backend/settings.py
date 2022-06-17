b52f23ebe54bfe270c38c9974dd10a79b2edf868
geoffn7 <geoffrey.nguyen9@gmail.com>
2022-06-17 19:07:10 -0400

    Connect store app to MySQL db store
    - Store secret key and db password to local file listed in .gitignore
    - Migrate models to store db

--- backend/gardenway_backend/gardenway_backend/settings.py
+++ backend/gardenway_backend/gardenway_backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from gardenway_backend.dev import DEV_SECRET_KEY, DB_PASSWORD
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dw&5b#gnyq)1nnpq*0^#o%-doz#d8zm&5z)5u3kzb*w+y*dyg8'
+SECRET_KEY = DEV_SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -76,8 +77,12 @@ WSGI_APPLICATION = 'gardenway_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'store',
+        'USER': 'root',
+        'PASSWORD': DB_PASSWORD,
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
 
