97c3c8c8f030396e4b6a142fe2fb306df3e6e585
geoffn7 <geoffrey.nguyen9@gmail.com>
2022-06-17 19:52:54 -0400

    Install & integrate django debug toolbar

--- backend/gardenway_backend/gardenway_backend/settings.py
+++ backend/gardenway_backend/gardenway_backend/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'store'
+    'store',
+    'debug_toolbar'
 ]
 
 MIDDLEWARE = [
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware'
 ]
 
 ROOT_URLCONF = 'gardenway_backend.urls'
@@ -86,6 +88,9 @@ DATABASES = {
     }
 }
 
+INTERNAL_IPS = [
+    '127.0.0.1'
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
--- backend/gardenway_backend/gardenway_backend/urls.py
+++ backend/gardenway_backend/gardenway_backend/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include
+import debug_toolbar
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('__debug__/', include(debug_toolbar.urls))
 ]
