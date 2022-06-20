f6c1697f887602b461d026cf2ff9cfc8c4cafad1
geoffn7 <geoffrey.nguyen9@gmail.com>
2022-06-20 01:08:15 -0400

    Implement delete option to /Products/<id>

--- backend/gardenway_backend/store/views.py
+++ backend/gardenway_backend/store/views.py
@@ -21,7 +21,7 @@ def product_list(request):
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
-@api_view(['GET', 'PUT', 'PATCH'])
+@api_view(['GET', 'PUT', 'PATCH', 'DELETE'])
 def product_detail(request, id):
     if request.method == 'GET':
         product = get_object_or_404(Product, pk=id)
@@ -32,3 +32,11 @@ def product_detail(request, id):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
+    elif request.method == 'DELETE':
+        product = get_object_or_404(Product, pk=id)
+        if product.orderitem_set.count() > 0:
+            return Response({'error': 'Product cannot be deleted because it is associated with an order item.'},
+                            status=status.HTTP_405_METHOD_NOT_ALLOWED)
+        else:
+            product.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
