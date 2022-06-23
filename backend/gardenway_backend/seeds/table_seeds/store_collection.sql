7265d2619b5e4c8253c49c56975dff573f962235
geoffn7 <geoffrey.nguyen9@gmail.com>
2022-06-23 01:45:28 -0400

    Fix and clean up seed.sql to populate db
    - Remove seed files for individual tables (redundant)
    - Fix store_cart insert statements to include customer id
      - In previous change, customer was added to cart model and seed file was not updated to match change
    - Fix duplicate insert statements for store_product_collections

--- backend/gardenway_backend/seeds/table_seeds/store_collection.sql
+++
@@ -1,5 +0,0 @@
-insert into store_collection (title, slug) values ('Rosaceae', 1);
-insert into store_collection (title, slug) values ('Solanaceae', 2);
-insert into store_collection (title, slug) values ('Chenopodiaceae', 3);
-insert into store_collection (title, slug) values ('Grossulariaceae', 4);
-insert into store_collection (title, slug) values ('Moraceae', 5);
