1aebbce553bf6cd11119f8e3ff5c209a9cbd4318
geoffn7 <geoffrey.nguyen9@gmail.com>
2022-06-30 01:47:24 -0400

    Implement owner and admin permissions to cart and order routes
    - Create custom permission classes for cart and order requests
    - Override in cart and order viewsets get_permissions() to implement custom logic
    git status
    - Only an admin can see the list of carts and orders and delete them
    - The owner can retrieve their own cart or order
    - Any authenticated user can create a cart or an order

--- backend/gardenway_backend/store/permissions.py
+++ backend/gardenway_backend/store/permissions.py
@@ -1,4 +1,6 @@
+from re import T
 from rest_framework import permissions
+from .serializers import OrderSerializer, CartSerializer
 
 
 class IsAdminOrReadOnly(permissions.BasePermission):
@@ -6,3 +8,25 @@ class IsAdminOrReadOnly(permissions.BasePermission):
         if request.method in permissions.SAFE_METHODS:  # GET, HEAD, OPTIONS
             return True
         return bool(request.user and request.user.is_staff)
+
+
+class IsAdminOrCartOwner(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.user.is_staff:
+            return True
+        elif request.method in ['OPTIONS', 'HEAD']:
+            return True
+        elif request.method == 'GET':
+            return (request.user.id == CartSerializer(obj).data['customer'])
+        return False
+
+
+class IsAdminOrOrderOwner(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.user.is_staff:
+            return True
+        if request.method in ['OPTIONS', 'HEAD']:
+            return True
+        elif request.method == 'GET':
+            return (request.user.id == OrderSerializer(obj).data['customer'])
+        return False
