32d2a0e02582caea543b7246eef978c17908f734
geoffn7 <geoffrey.nguyen9@gmail.com>
2022-07-01 21:25:50 -0400

    Implement rest of create collection test cases
    - Non-admin -> 403
    - Invalid data -> 400

--- backend/gardenway_backend/store/tests/test_collections.py
+++ backend/gardenway_backend/store/tests/test_collections.py
@@ -6,6 +6,12 @@ import pytest
 
 @pytest.mark.django_db
 class TestCreateCollection:
+
+    def get_valid_data(self):
+        return {
+            "title": "test"
+        }
+
     def test_returns_201(self):
 
         # Arrange
@@ -13,8 +19,31 @@ class TestCreateCollection:
         client.force_authenticate(user=User(is_staff=True))
 
         # Act
-        response = client.post('/store/collections/', {'title': 'a'})
+        response = client.post('/store/collections/', self.get_valid_data())
 
         # Assert
         assert response.status_code == status.HTTP_201_CREATED
         assert response.data['id'] > 0
+
+    def test_returns_403_from_non_admin(self):
+
+        # Arrange
+        client = APIClient()
+
+        # Act
+        response = client.post('/store/collections/', self.get_valid_data())
+
+        # Assert
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+
+    def test_returns_400_from_invalid_data(self):
+
+        # Arrange
+        client = APIClient()
+        client.force_authenticate(user=User(is_staff=True))
+
+        # Act
+        response = client.post('/store/collections/', {"bread": "bread"})
+
+        # Assert
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
