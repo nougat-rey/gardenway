a5e24b842a937d800c0577763fffbdccb5aaf415
geoffn7 <geoffrey.nguyen9@gmail.com>
2022-07-02 03:18:41 -0400

    Implement create cart tests
    - Install model_baker to help with populating database to arrange for testing
    - To create a cart, a customer must exist in the database, model_baker helps easily create a customer entry

--- backend/gardenway_backend/store/tests/test_carts.py
+++ backend/gardenway_backend/store/tests/test_carts.py
@@ -2,3 +2,61 @@ from rest_framework import status
 from rest_framework.test import APIClient
 from store.models import User
 import pytest
+from store.models import Customer
+from model_bakery import baker
+import uuid
+
+
+def is_valid_uuid(input):
+    try:
+        uuid.UUID(str(input))
+        return True
+    except ValueError:
+        return False
+
+
+@pytest.mark.django_db
+class TestCreateCart:
+
+    url = '/store/carts/'
+
+    def get_valid_data(self):
+        return {
+            "customer": 1
+        }
+
+    def test_returns_201(self):
+
+        # Arrange
+        client = APIClient()
+        client.force_authenticate(user=User(is_staff=True))
+        customer = baker.make(Customer)
+
+        # Act
+        response = client.post(self.url, self.get_valid_data())
+
+        # Assert
+        print(response.data)
+        assert response.status_code == status.HTTP_201_CREATED
+        assert is_valid_uuid(response.data['id'])
+
+    def test_returns_403_from_anonymous(self):
+        # Arrange
+        client = APIClient()
+
+        # Act
+        response = client.post(self.url, self.get_valid_data())
+
+        # Assert
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+
+    def test_returns_400_from_invalid_data(self):
+        # Arrange
+        client = APIClient()
+        client.force_authenticate(user=User(is_staff=True))
+
+        # Act & Assert
+
+        # 1. customer does not exist
+        response = client.post(self.url, {"customer": 999})
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
