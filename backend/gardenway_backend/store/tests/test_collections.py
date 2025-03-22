a1f933af9f23d0b9fdad44f0fb8af708999c40b1
geoffn7 <geoffrey.nguyen9@gmail.com>
2025-03-22 13:51:45 -0400

    Refactor pytest for collections

--- backend/gardenway_backend/store/tests/test_collections.py
+++ backend/gardenway_backend/store/tests/test_collections.py
@@ -5,9 +5,30 @@ from model_bakery import baker
 import pytest
 
 
+@pytest.fixture
+def admin_user():
+    return baker.make(User, is_staff=True)
+
+
+@pytest.fixture
+def non_admin_user():
+    return baker.make(User, is_staff=False)
+
+
+@pytest.fixture
+def authenticated_client(admin_user):
+    client = APIClient()
+    client.force_authenticate(user=admin_user)
+    return client
+
+
+@pytest.fixture
+def non_authenticated_client():
+    return APIClient()
+
+
 @pytest.mark.django_db
 class TestCreateCollection:
-
     url = '/store/collections/'
 
     def get_valid_data(self):
@@ -16,24 +37,18 @@ class TestCreateCollection:
             "slug": "test"
         }
 
-    def test_returns_201(self):
-
-        # Arrange
-        client = APIClient()
-        client.force_authenticate(user=User(is_staff=True))
-
+    def test_returns_201(self, authenticated_client):
         # Act
-        response = client.post(self.url, self.get_valid_data())
+        response = authenticated_client.post(self.url, self.get_valid_data())
 
         # Assert
         assert response.status_code == status.HTTP_201_CREATED
         assert response.data['id'] > 0
 
-    def test_returns_403_from_non_admin(self):
-
+    def test_returns_403_from_non_admin(self, non_admin_user):
         # Arrange
         client = APIClient()
-        client.force_authenticate(user=User(is_staff=False))
+        client.force_authenticate(user=non_admin_user)
 
         # Act
         response = client.post(self.url, self.get_valid_data())
@@ -41,14 +56,9 @@ class TestCreateCollection:
         # Assert
         assert response.status_code == status.HTTP_403_FORBIDDEN
 
-    def test_returns_400_from_invalid_data(self):
-
-        # Arrange
-        client = APIClient()
-        client.force_authenticate(user=User(is_staff=True))
-
+    def test_returns_400_from_invalid_data(self, authenticated_client):
         # Act
-        response = client.post(self.url, {"bread": "bread"})
+        response = authenticated_client.post(self.url, {"bread": "bread"})
 
         # Assert
         assert response.status_code == status.HTTP_400_BAD_REQUEST
@@ -58,13 +68,9 @@ class TestCreateCollection:
 class TestListCollections:
     url = '/store/collections/'
 
-    def test_returns_200(self):
-
-        # Arrange
-        client = APIClient()
-
+    def test_returns_200(self, non_authenticated_client):
         # Act
-        response = client.get(self.url)
+        response = non_authenticated_client.get(self.url)
 
         # Assert
         assert response.status_code == status.HTTP_200_OK
@@ -73,24 +79,19 @@ class TestListCollections:
 @pytest.mark.django_db
 class TestGetCollection:
 
-    def test_returns_200(self):
-
+    def test_returns_200(self, non_authenticated_client):
         # Arrange
-        client = APIClient()
         collection = baker.make(Collection)
 
         # Act
-        response = client.get(f'/store/collections/{collection.id}/')
+        response = non_authenticated_client.get(f'/store/collections/{collection.id}/')
 
         # Assert
         assert response.status_code == status.HTTP_200_OK
 
-    def test_returns_404(self):
-        # Arrange
-        client = APIClient()
-
+    def test_returns_404(self, non_authenticated_client):
         # Act
-        response = client.get(f'/store/collections/500/')
+        response = non_authenticated_client.get(f'/store/collections/500/')
 
         # Assert
         assert response.status_code == status.HTTP_404_NOT_FOUND
