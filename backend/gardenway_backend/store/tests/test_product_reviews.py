498bbccff5f29c21a23b65d5b7c0325b5ba57594
geoffn7 <geoffrey.nguyen9@gmail.com>
2025-03-22 13:08:33 -0400

    Refactor pytest product reviews using fixtures

--- backend/gardenway_backend/store/tests/test_product_reviews.py
+++ backend/gardenway_backend/store/tests/test_product_reviews.py
@@ -1,60 +1,64 @@
+import pytest
 from rest_framework import status
 from rest_framework.test import APIClient
 from store.models import User, Product
 from model_bakery import baker
-import pytest
+
+
+@pytest.fixture
+def client():
+    return APIClient()
+
+
+@pytest.fixture
+def user():
+    return baker.make(User, is_staff=False)
+
+
+@pytest.fixture
+def admin_user():
+    return baker.make(User, is_staff=True)
+
+
+@pytest.fixture
+def product():
+    return baker.make(Product)
+
+
+@pytest.fixture
+def review_data(product):
+    return {
+        "product": product.id,
+        "rating": 5,
+        "name": "Great Product",
+        "description": "I really loved this product. It exceeded my expectations!",
+        "date": "2022-05-30"
+    }
+
 
 @pytest.mark.django_db
 class TestCreateProductReview:
 
-    def test_returns_201(self):
-
+    def test_returns_201(self, client, user, product, review_data):
         # Arrange
-        client = APIClient()
-        user = baker.make(User, is_staff=False)
-        client.force_authenticate(user)
-        product = baker.make(Product)
-        data = {
-            "product": product.id,
-            "rating": 5,
-            "name": "Great Product",
-            "description": "I really loved this product. It exceeded my expectations!",
-            "date": "2022-05-30"
-        }
+        client.force_authenticate(user=user)
 
         # Act
-        response = client.post(f'/store/products/{product.id}/reviews/', data)
+        response = client.post(f'/store/products/{product.id}/reviews/', review_data)
 
         # Assert
         assert response.status_code == status.HTTP_201_CREATED
 
-    def test_returns_403_from_anonymous(self):
-
-        # Arrange
-        client = APIClient()
-        user = baker.make(User, is_staff=False)
-        product = baker.make(Product)
-        data = {
-            "product": product.id,
-            "rating": 5,
-            "name": "Great Product",
-            "description": "I really loved this product. It exceeded my expectations!",
-            "date": "2022-05-30"
-        }
-
+    def test_returns_403_from_anonymous(self, client, product, review_data):
         # Act
-        response = client.post(f'/store/products/{product.id}/reviews/', data)
+        response = client.post(f'/store/products/{product.id}/reviews/', review_data)
 
         # Assert
         assert response.status_code == status.HTTP_403_FORBIDDEN
 
-    def test_returns_400_from_invalid_data(self):
-        
+    def test_returns_400_from_invalid_data(self, client, user, product):
         # Arrange
-        client = APIClient()
-        user = baker.make(User, is_staff=False)
-        client.force_authenticate(user)
-        product = baker.make(Product)
+        client.force_authenticate(user=user)
         data = {
             "rating": 5,
             "name": "Great Product",
@@ -72,12 +76,9 @@ class TestCreateProductReview:
 @pytest.mark.django_db
 class TestListProductReviews:
 
-    def test_returns_200(self):
-
+    def test_returns_200(self, client, user, product):
         # Arrange
-        client = APIClient()
-        client.force_authenticate(user=User(is_staff=False))
-        product = baker.make(Product)
+        client.force_authenticate(user=user)
 
         # Act
         response = client.get(f'/store/products/{product.id}/reviews/')
