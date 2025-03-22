561369f92cceba6211817ef4aabc3661608f6e19
geoffn7 <geoffrey.nguyen9@gmail.com>
2025-03-22 13:50:02 -0400

    Refactor pytest for customers

--- backend/gardenway_backend/store/tests/test_customers.py
+++ backend/gardenway_backend/store/tests/test_customers.py
@@ -4,30 +4,35 @@ from store.models import User
 from model_bakery import baker
 import pytest
 
+@pytest.fixture
+def authenticated_client():
+    client = APIClient()
+    user = baker.make(User, is_staff=True)
+    client.force_authenticate(user)
+    return client, user
+
+
 @pytest.mark.django_db
 class TestCreateCustomer:
 
     url = '/store/customers/'
 
     @pytest.mark.skip(reason="Customer automatically created due to signals/handlers")
-    def test_returns_201(self):
-
-        # Arrange
-        client = APIClient()
-        user = baker.make(User, is_staff=True)
-        client.force_authenticate(user)
+    def test_returns_201(self, authenticated_client):
+        client, user = authenticated_client
 
         # Act
         response = client.post(self.url, {"user_id": user.id})
+
+        # Debugging: Print the response content to see the error message
+        print(response.data)
 
         # Assert
         assert response.status_code == status.HTTP_201_CREATED
         assert response.data['id'] > 0
 
-    @pytest.mark.skip(reason="Customer automatically created due to signals/handlers")
+
     def test_returns_403_from_non_admin(self):
-        
-        # Arrange
         client = APIClient()
         user = baker.make(User, is_staff=False)
         client.force_authenticate(user)
@@ -38,24 +43,17 @@ class TestCreateCustomer:
         # Assert
         assert response.status_code == status.HTTP_403_FORBIDDEN
 
-    def test_returns_400_from_invalid_user_id(self):
-        
-        # Arrange
-        client = APIClient()
-        user = baker.make(User, is_staff=True)
-        client.force_authenticate(user)
+    def test_returns_400_from_invalid_user_id(self, authenticated_client):
+        client, _ = authenticated_client
 
         # Act
-        response = client.post(self.url, {"user_id": 500}) # User does not exist
+        response = client.post(self.url, {"user_id": 500})  # User does not exist
         
         # Assert
         assert response.status_code == status.HTTP_400_BAD_REQUEST
 
-    def test_returns_400_from_no_user_id(self):
-        # Arrange
-        client = APIClient()
-        user = baker.make(User, is_staff=True)
-        client.force_authenticate(user)
+    def test_returns_400_from_no_user_id(self, authenticated_client):
+        client, _ = authenticated_client
 
         # Act
         response = client.post(self.url, {})
@@ -63,29 +61,23 @@ class TestCreateCustomer:
         # Assert
         assert response.status_code == status.HTTP_400_BAD_REQUEST
 
-    def test_returns_400_if_customer_exists(self):
-
-        # Arrange
-        client = APIClient()
-        user = baker.make(User, is_staff=True)
-        client.force_authenticate(user)
+    def test_returns_400_if_customer_exists(self, authenticated_client):
+        client, user = authenticated_client
 
         # Act
-        # Customer is already created above when the user was created
+        # Customer is already created when the user was created
         response = client.post(self.url, {"user_id": user.id})
         
         # Assert
         assert response.status_code == status.HTTP_400_BAD_REQUEST
 
+
 @pytest.mark.django_db
 class TestListCustomers:
     url = '/store/customers/'
 
-    def test_returns_200(self):
-
-        # Arrange
-        client = APIClient()
-        client.force_authenticate(user=User(is_staff=True))
+    def test_returns_200(self, authenticated_client):
+        client, _ = authenticated_client
 
         # Act
         response = client.get(self.url)
@@ -94,8 +86,6 @@ class TestListCustomers:
         assert response.status_code == status.HTTP_200_OK
 
     def test_returns_403_from_non_admin(self):
-
-        # Arrange
         client = APIClient()
         client.force_authenticate(user=User(is_staff=False))
 
@@ -109,37 +99,29 @@ class TestListCustomers:
 @pytest.mark.django_db
 class TestGetCustomer:
 
-    def test_returns_200(self):
-
-        # Arrange
-        client = APIClient()
-        user = baker.make(User, is_staff=True)
-        client.force_authenticate(user)
+    def test_returns_200(self, authenticated_client):
+        client, user = authenticated_client
 
         # Act
-        response = client.get(f'/store/customers/me/', {"user_id":user.id})
-        print(response)
+        response = client.get(f'/store/customers/me/', {"user_id": user.id})
+
         # Assert
         assert response.status_code == status.HTTP_200_OK
         assert response.data['id'] > 0
 
     def test_returns_200_from_self(self):
-        # Arrange
         client = APIClient()
         user = baker.make(User, is_staff=False)
         client.force_authenticate(user)
 
         # Act
-        response = client.get(f'/store/customers/me/', {"user_id":user.id})
+        response = client.get(f'/store/customers/me/', {"user_id": user.id})
 
         # Assert
         assert response.status_code == status.HTTP_200_OK
-        
-    def test_returns_404_client_does_not_exist(self):
-        # Arrange
-        client = APIClient()
-        user = baker.make(User, is_staff=True)
-        client.force_authenticate(user)
+
+    def test_returns_404_client_does_not_exist(self, authenticated_client):
+        client, _ = authenticated_client
 
         # Act
         response = client.get(f'/store/customers/500/')
