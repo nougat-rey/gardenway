6bb1143bcc9c77c3ebaf5d563577a466439f5c81
geoffn7 <geoffrey.nguyen9@gmail.com>
2025-03-22 19:30:23 -0400

    Update prod settings to look for Heroku MariaDB

--- backend/gardenway_backend/gardenway_backend/settings/prod.py
+++ backend/gardenway_backend/gardenway_backend/settings/prod.py
@@ -1,6 +1,11 @@
 import os
 from .common import *
+import dj_database_url
 
 DEBUG = False
 ALLOWED_HOSTS = ["https://gardenway-2d10f268a969.herokuapp.com/"]
 SECRET_KEY = os.environ['SECRET_KEY']
+
+DATABASES = {
+        'default':dj_database_url.config()
+    }
