b28f1d47637d829a08a3d5f6805f79498b6d8c8b
geoffn7 <geoffrey.nguyen9@gmail.com>
2025-03-22 21:38:24 -0400

    Fix typo for STATIC_URL and add STATICFILES_STORAGE

--- settings/common.py
+++ settings/common.py
@@ -141,10 +141,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# WhiteNoise configuration to allow efficient serving of static files
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
