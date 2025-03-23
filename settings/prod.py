62fd96be1806bf8793108c41caf46c598e545a0d
geoffn7 <geoffrey.nguyen9@gmail.com>
2025-03-22 21:24:29 -0400

    Update ALLOWED_HOSTS

--- settings/prod.py
+++ settings/prod.py
@@ -3,7 +3,8 @@ from .common import *
 import dj_database_url
 
 DEBUG = False
-ALLOWED_HOSTS = ["https://gardenway-4e86e7894057.herokuapp.com/"]
+ALLOWED_HOSTS = ['gardenway-4e86e7894057.herokuapp.com']
+
 SECRET_KEY = os.environ['SECRET_KEY']
 
 DATABASES = {
