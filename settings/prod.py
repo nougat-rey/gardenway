953fd2c4ab4a3aaf88f90d7c64bb71791685d994
geoffn7 <geoffrey.nguyen9@gmail.com>
2025-04-07 01:22:11 -0400

    Update MEDIA URL to use Cloudinary

--- settings/prod.py
+++ settings/prod.py
@@ -23,6 +23,10 @@ CLOUDINARY_STORAGE = {
     'CLOUD_NAME': env('CLOUDINARY_CLOUD_NAME'),
     'API_KEY': env('CLOUDINARY_API_KEY'),
     'API_SECRET': env('CLOUDINARY_API_SECRET'),
+    'CACHEABLE': True,  # To enable caching for faster delivery of media URLs
 }
+# Cloudinary settings
+MEDIA_URL = 'https://res.cloudinary.com/{}/'.format(os.getenv('CLOUDINARY_CLOUD_NAME'))
+
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
