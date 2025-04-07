de6bd70df754a2be54f967a475efe8d4631c18cd
geoffn7 <geoffrey.nguyen9@gmail.com>
2025-04-07 01:54:53 -0400

    Update MEDIA_URL

--- settings/prod.py
+++ settings/prod.py
@@ -6,27 +6,31 @@ import cloudinary.uploader
 import cloudinary.api
 import environ
 
+# Load environment variables from .env
 env = environ.Env()
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))  # Optional if using .env
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
-
+# Production settings
 DEBUG = False
 ALLOWED_HOSTS = ['gardenway-4e86e7894057.herokuapp.com']
 
 SECRET_KEY = os.environ['SECRET_KEY']
 
+# Database settings for production
 DATABASES = {
-        'default':dj_database_url.config()
-    }
+    'default': dj_database_url.config()
+}
 
+# Cloudinary settings for production
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': env('CLOUDINARY_CLOUD_NAME'),
     'API_KEY': env('CLOUDINARY_API_KEY'),
     'API_SECRET': env('CLOUDINARY_API_SECRET'),
     'CACHEABLE': True,  # To enable caching for faster delivery of media URLs
 }
-# Cloudinary settings
+
+# Set MEDIA_URL to directly reference Cloudinary
 MEDIA_URL = 'https://res.cloudinary.com/{}/'.format(os.getenv('CLOUDINARY_CLOUD_NAME'))
 
-
+# Use Cloudinary for media storage
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
