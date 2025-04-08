48eaa7f8caf7a77d169469cb8f0b42a9440d000e
geoffn7 <geoffrey.nguyen9@gmail.com>
2025-04-07 22:06:15 -0400

    Update prod.py to get settings from environment variables instead of .env file as the .env file is not pushed via git

--- settings/prod.py
+++ settings/prod.py
@@ -4,19 +4,18 @@ import dj_database_url
 import cloudinary
 import cloudinary.uploader
 import cloudinary.api
-import environ
-
-# Load environment variables from .env
-env = environ.Env()
-env = environ.Env()
-env_file_path = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), '.env')
-environ.Env.read_env(env_file_path)
+from django.core.exceptions import ImproperlyConfigured
 
 # Production settings
 DEBUG = False
 ALLOWED_HOSTS = ['https://gardenway-11a7983dd747.herokuapp.com']
 
-SECRET_KEY = env('SECRET_KEY')
+# Fetch the SECRET_KEY from environment variables
+SECRET_KEY = os.getenv('SECRET_KEY')
+
+# Check if SECRET_KEY is missing (good for debugging)
+if not SECRET_KEY:
+    raise ImproperlyConfigured("The SECRET_KEY setting must not be empty.")
 
 # Database settings for production
 DATABASES = {
@@ -25,12 +24,11 @@ DATABASES = {
 
 # Cloudinary settings for production
 CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': env('CLOUDINARY_CLOUD_NAME'),
-    'API_KEY': env('CLOUDINARY_API_KEY'),
-    'API_SECRET': env('CLOUDINARY_API_SECRET'),
+    'CLOUD_NAME': os.getenv('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': os.getenv('CLOUDINARY_API_KEY'),
+    'API_SECRET': os.getenv('CLOUDINARY_API_SECRET'),
     'CACHEABLE': True,  # To enable caching for faster delivery of media URLs
 }
 
 # Use Cloudinary for media storage
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
-
